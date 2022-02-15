3ea1657ce17b8432c3c9555217ba65bb14190c63
Ilnur <nabiulli@mail.ru>
2022-02-15 13:23:10 +0500

    Выполнил домашнее задание по теме строки

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -1,8 +1,30 @@
 package ru.skypro;
 
+import java.util.Locale;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	// Задание 1
+        String firstName = "Ivan";
+        String middleName = "ivanovich";
+        String lastName = "Ivanov";
+        String space = " ";
+        String fullName = lastName + space + firstName + space + middleName;
+        System.out.println("ФИО сотрудника - " + fullName);
+
+        // Задание 2
+        String fullNameCapitalLetters = fullName.toUpperCase();
+        System.out.println("Данные ФИО сотрудника для заполнения отчета - " + fullNameCapitalLetters);
+
+        // Задание 3
+        String fullNameSemicolon = fullName.replace(" ", "; ");
+        System.out.println("Данные ФИО сотрудника для административного отдела - " + fullNameSemicolon);
+
+        // Задание 4
+
+        String fullName2 = "Иванов Семён Семёнович";
+        String fullName2Without = fullName2.replace("ё", "е");
+        System.out.println("Данные ФИО сотрудника - " + fullName2Without);
     }
 }
